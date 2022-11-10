488cc761ca25a13f42cfe4478b84ab7b5790e216
Matt McCallion <matt.mccallion@digital.cabinet-office.gov.uk>
2022-11-10 11:21:34 +0000

    LIME-240 - Fixes Postcode validation triggers.
    Signed-off-by: Matt McCallion <matt.mccallion@digital.cabinet-office.gov.uk>

--- src/app/drivingLicence/fields.js
+++ src/app/drivingLicence/fields.js
@@ -182,7 +182,9 @@ module.exports = {
       "required",
       { type: "maxlength", arguments: [8] },
       { type: "minlength", arguments: [5] },
-      { type: "regexPostcodeAlpha", fn: (value) => value.match(/^[A-Za-z0-9 ]*$/) },
+      { type: "regexPostcodeAlpha", fn: (value) => value.match(/[A-Za-z]+/) },
+      { type: "regexPostcodeAlpha", fn: (value) => value.match(/[0-9]+/) },
+      { type: "regexPostcodeSymbol", fn: (value) => value.match(/^[A-Za-z0-9 ]+$/) },
       { type: "regexPostcodeUK", fn: (value) => value.match(/([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/) }
     ],
     classes: "govuk-input--width-10",
