5ba572ffdc5458aa2d93d7e5006aaed420493e62
Rachel Roberts <rachel.roberts@digital.cabinet-office.gov.uk>
2024-10-23 11:17:53 +0100

    LIME-1311 check details screen added
    LIME-1311 check details screen added
    
    LIME-1311 add check your details screen

---
+++ src/app/drivingLicence/controllers/check-your-details.js
@@ -0,0 +1,39 @@
+const BaseController = require("hmpo-form-wizard").Controller;
+const logger = require("hmpo-logger").get();
+
+class CheckYourDetailsController extends BaseController {
+  async saveValues(req, res, next) {
+    try {
+      req.sessionModel.set("detailsNotConfirmed", undefined);
+      const action = req.form.values.confirmDetails;
+
+      switch (action) {
+        case "detailsNotConfirmed": {
+          logger.info(
+            "check-your-details: User has indicated details are incorrect, routing back to IPVCore",
+            { req, res }
+          );
+          req.sessionModel.set("detailsNotConfirmed", true);
+          return next();
+        }
+        case "detailsConfirmed": {
+          logger.info(
+            "check-your-details: User has confirmed details : redirecting to driving licence API",
+            {
+              req,
+              res
+            }
+          );
+          return next();
+        }
+      }
+      return next(new Error("check-your-details: Invalid action - " + action));
+    } catch (err) {
+      logger.error(
+        "check-your-details: Unable to set session model error - " + err
+      );
+      return next(err);
+    }
+  }
+}
+module.exports = CheckYourDetailsController;
